678ac8e8b30a5ebea9fa2fbbafd99222c75cfbfd
funkygao <funky.gao@gmail.com>
2014-10-23 23:47:41 +0800

    a basic mem pool

--- mempool.go
+++ mempool.go
@@ -1,3 +1,15 @@
 package mqttmsg
 
-// TODO
+import (
+	"bytes"
+	"github.com/funkygao/golib/recycler"
+)
+
+var (
+	bufferPoolGet, bufferPoolGive = recycler.New(100, func() interface{} {
+		return new(bytes.Buffer)
+	})
+	bytesPoolGet, bytesPoolPut = recycler.New(100, func() interface{} {
+		return make([]byte, 100<<10) // max 100KB
+	})
+)
